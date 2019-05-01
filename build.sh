351323bc5607f67dc384455c8c98c71eb52c5bf7
Domingo Suarez Torres <domingo.suarez@gmail.com>
2019-05-01 12:34:32 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-VER=$(grep "version" website.properties|cut -d'=' -f2)  && docker build -t domix/website:$VER . && docker push domix/website:$VER #&& kubectl set image deployment/website website=domix/website:$VER'
+VER=$(grep "version" website.properties|cut -d'=' -f2)  && docker build -t domix/website:$VER . #&& docker push domix/website:$VER #&& kubectl set image deployment/website website=domix/website:$VER'
